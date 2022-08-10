2392f6f6953a31a93958f406093c9bfc33054bc4
Dingwen Wang <wangdingwen32@gmail.com>
2022-08-10 06:50:17 -0400

    Update

--- auto-archive.bat
+++ auto-archive.bat
@@ -1,28 +1,41 @@
 @echo off
 
-set AF="Archive"
+SET AF="Archive"
 
-if [%1]==[] goto run
+IF [%1]==[] GOTO RUN
 
-set fn=%1
-IF ["%fn:"=%"]==["%~n0%~x0"] exit
-IF [%1]==[%AF%] exit
+ECHO Processing %1...
 
-FOR /F "tokens=1,2 delims=/ " %%i in ("%2") do set yeardate="%%i-%%j"
+SET fn=%1
+IF ["%fn:"=%"]==["%~n0%~x0"] EXIT
+IF [%1]==[%AF%] EXIT
 
-set y=%date:~0,4%
-set /a m=%date:~5,2%
-set cym="%y%-%m%"
-IF %yeardate%==%cym% exit
+FOR /F "tokens=1,2 delims=/ " %%i in ("%2") do (
+    SET year="%%i"
+    SET month="%%j"
+)
+IF %month% LEQ 9 SET month=0%month%
+SET yeardate="%year%-%month%"
 
 IF NOT EXIST .\%AF% MKDIR %AF%
 IF NOT EXIST .\%AF%\%yeardate% MKDIR %AF%\%yeardate%
+IF NOT EXIST .\%AF%\LOG MKDIR %AF%\LOG
 
-echo %fn:"=%
+SET logfile=.\%AF%\LOG\%yeardate%.log
+IF NOT EXIST %logfile% ECHO [%date% %time% %USERNAME%@%COMPUTERNAME%] Log file created.>%logfile%
+SET message=ERR
+
+IF EXIST .\%AF%\%yeardate%\%1 GOTO DONE
+
 MOVE /-Y %1 .\%AF%\%yeardate%\%1
+SET message=OK
 
-exit
+:DONE
+ECHO [%date% %time% %USERNAME%@%COMPUTERNAME%] %message%: Move "%fn:"=%" to "%AF:"=%\%yeardate:"=%\%fn:"=%">>%logfile%
+ECHO %message%: %fn:"=%
 
-:run
-forfiles /C "cmd /c %0 @file @fdate @ftime"
-pause
+EXIT
+
+:RUN
+FORFILES /D -30 /C "cmd /c %0 @file @fdate @ftime"
+PAUSE
